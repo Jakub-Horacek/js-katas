b89c36fd58ccd51502b8d4a546164e8b140af012
Jakub Horáček <jakub.horacek@elixeum.com>
2023-09-07 23:12:33 +0200

    Code cleanup (with comments)

--- kata1-calculator/src/calculator.js
+++ kata1-calculator/src/calculator.js
@@ -29,6 +29,11 @@ let commaAlreadyPlaced = false;
 const root = document.querySelector(":root");
 const defaultResultSize = getComputedStyle(root).getPropertyValue("--result-number-size");
 
+/**
+ *  Add provided char into the string equation
+ * @param {string} char to add into the result innerText
+ * @returns when trying to place already existing comma
+ */
 const addCharacter = (char) => {
   if (lastEquationSolved) {
     allClear();
@@ -43,10 +48,9 @@ const addCharacter = (char) => {
   checkResultLength();
 };
 
-const isResultContainingComma = () => {
-  return getResultInnerText().includes(".");
-};
-
+/**
+ * Delete the last char of the result innerText
+ */
 const removeCharacter = () => {
   let resultString = getResultInnerText();
   setResultInnerText(resultString.slice(0, -1));
@@ -54,12 +58,18 @@ const removeCharacter = () => {
   checkResultLength();
 };
 
+/**
+ * Clear the result innerText
+ */
 const allClear = () => {
   setResultInnerText("");
   root.style.setProperty("--result-number-size", defaultResultSize);
   commaAlreadyPlaced = false;
 };
 
+/**
+ * Adjust the result font size by the result innerText length
+ */
 const checkResultLength = () => {
   let resultLength = getResultInnerText().length;
 
@@ -80,16 +90,60 @@ const checkResultLength = () => {
   }
 };
 
+/**
+ * Get the result innerText
+ * @returns the innerText of the result HTML element
+ */
 const getResultInnerText = () => {
   return document.getElementById("result").innerText;
 };
 
+/**
+ * Display the given string in the result
+ * @param {string} text which will be displayed in the result
+ */
 const setResultInnerText = (text) => {
   document.getElementById("result").innerText = text;
 };
 
+/**
+ *  Check the result if it is already containg a comma
+ * @returns true when result is containg comma
+ */
+const isResultContainingComma = () => {
+  return getResultInnerText().includes(".");
+};
+
+/**
+ *  Check if the string is containing any operators
+ * @param {string} equation
+ * @returns true when not containing at least one of the 4 logical operators
+ */
+const notContainingAnyOperators = (equation) => {
+  return !(equation.includes("/") || equation.includes("*") || equation.includes("+") || equation.includes("-"));
+};
+
+/**
+ *  Check if the equation is valid
+ * @param {string} equation
+ * @returns true when the equation is not valid
+ */
+const notValidEquation = (equation) => {
+  return equation == "" || notContainingAnyOperators(equation);
+};
+
+/**
+ * Transform the resultString into a math equation and solve the equation
+ * @returns when the equation is not valid
+ */
 const solve = () => {
   let resultString = getResultInnerText();
+
+  if (notValidEquation(resultString)) {
+    console.log("not valid");
+    return;
+  }
+
   let solvedEquation = eval(resultString);
   setResultInnerText(solvedEquation);
   lastEquationSolved = true;
