896f1cd10e2ae832eb7541efd19b69377bfcb5f6
Jakub Horáček <jakub.horacek@elixeum.com>
2023-11-15 19:11:04 +0100

    Add card generating by count and card flipping

--- kata3-memory-game/src/app.js
+++ kata3-memory-game/src/app.js
@@ -1,5 +1,6 @@
 // NOTE: Number of cards the player plays with (this number needs to be even)
-let playcardsCount = 12;
+let playcardsCount = 8;
+const maximumPlaycardsCount = 20;
 
 const gamegrid = document.querySelector("#playgrid");
 
@@ -8,10 +9,29 @@ if ((playcardsCount | 1) === playcardsCount) {
   playcardsCount = 2 * Math.round(playcardsCount / 2);
 }
 
+// Make sure the playcardsCount is lower than maximumPlaycardsCount
+if (playcardsCount >= maximumPlaycardsCount) {
+  playcardsCount = maximumPlaycardsCount;
+}
+
+const getUniqueId = () => {
+  return Date.now().toString(32) + Math.random().toString(16).replace(".", "");
+};
+
+const flipCard = (cardId) => {
+  const card = document.getElementById(cardId);
+  const sideA = card.querySelector(".playcard__side-a");
+  const sideB = card.querySelector(".playcard__side-b");
+
+  sideA.classList.toggle("playcard__side--visible");
+  sideB.classList.toggle("playcard__side--visible");
+};
+
 const renderPlaycard = () => {
   const card = document.createElement("div");
+  card.id = getUniqueId();
   card.className = "playcard";
-  card.addEventListener("click", () => flipCard());
+  card.addEventListener("click", () => flipCard(card.id));
 
   const sideA = document.createElement("div");
   sideA.className = "playcard__side playcard__side-a playcard__side--visible";
@@ -24,4 +44,10 @@ const renderPlaycard = () => {
   return card;
 };
 
-gamegrid.appendChild(renderPlaycard());
+const renderPlayercards = (count) => {
+  for (let i = 0; i < count; i++) {
+    gamegrid.appendChild(renderPlaycard());
+  }
+};
+
+renderPlayercards(playcardsCount);
